8bf4b50ca731589dd612a45b8549c5ff341bd3d3
Dong-Shiyu-NJU <2656304384@qq.com>
2025-06-10 16:42:18 +0800

    change mrct's g to sliced g

--- ct/mrct.py
+++ ct/mrct.py
@@ -3,21 +3,20 @@ import numpy as np
 from ct.get_cabs import get_cabs
 from ct.utils.orbital_space import OrbitalSpace
 from ct.get_int import get_eri_ri_ri_int,get_hcore_int,get_density,get_fock
-from ct.get_f12_int import get_f12,gen_V,get_fock_ri,gen_b,rational_generate,conjugate
+from ct.get_f12_int import get_f12,gen_V,gen_b,rational_generate,conjugate
 from ct.get_hbar import get_hbar
+from ct.utils.eri_interface import SlicedERI
 def canonical_transform(mol,wfn,basis,df_basis,gamma,freeze_core,mr_info):
 
     obs,ribs,cabs=get_cabs(mol,wfn,basis,df_basis)
     my_orbital_space=OrbitalSpace(wfn,obs,ribs,cabs)
-    g=get_eri_ri_ri_int(my_orbital_space)
     sliced_g=SlicedERI(my_orbital_space)
     D1,D2=get_density(my_orbital_space,mr_info)
     h=get_hcore_int(my_orbital_space)
-    f=get_fock(my_orbital_space,h,D1,sliced_g.format_g_for_fock())
+    fock_only_j,k,f=get_fock(my_orbital_space,h,D1,sliced_g.format_g_for_fock())
     G=get_f12(gamma,my_orbital_space)
-    V_noper,X_noper=gen_V(gamma,my_orbital_space)
-    fock_ri_mo,K_ri_mo,total_fock,f_virtual_cabs=get_fock_ri(my_orbital_space)
-    B_final_temp=gen_b(gamma,my_orbital_space,total_fock,fock_ri_mo,K_ri_mo)
+    V_noper,X_noper=gen_V(gamma,sliced_g,my_orbital_space)
+    B_final_temp=gen_b(gamma,my_orbital_space,f,fock_only_j,k)
     if freeze_core:
         nfrzc=wfn.nfrzc()
         try :
@@ -39,7 +38,7 @@ def canonical_transform(mol,wfn,basis,df_basis,gamma,freeze_core,mr_info):
     B_rational_temp=rational_generate(B_final_temp)
     B_rational=conjugate(rational_generate(conjugate(B_rational_temp)))
 
-    hbar,gbar=get_hbar(my_orbital_space,V_rational,X_rational,B_rational,D1,D2,g,G,f,h)
+    hbar,gbar=get_hbar(my_orbital_space,V_rational,X_rational,B_rational,D1,D2,sliced_g,G,f,h)
     Cp=my_orbital_space.Cp
     mints=psi4.core.MintsHelper(my_orbital_space.bs_obs())
     Cinv = Cp.T @ mints.ao_overlap()
